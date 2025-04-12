f5ba4b99c0e92d551c585563641e33b16997b0f7
Matt <matt.mill@valueiron.com>
2025-04-12 16:26:35 -0600

    Additional Output Variables

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,34 @@
 output "vm_ip" {
-  description = "List of VM IP addresses"
+  description = "VM IP Addresses"
   value       = [for vm in proxmox_vm_qemu.pvevm : vm.default_ipv4_address]
 }
 
+output "vm_nameserver" {
+  description = "VM Nameservers"
+  value       = [for vm in proxmox_vm_qemu.pvevm : vm.nameserver]
+}
+
 output "vm_name" {
-  description = "List of VM names"
+  description = "VM NAMES"
   value       = [for vm in proxmox_vm_qemu.pvevm : vm.name]
 }
 
 output "vm_id" {
-  description = "List of VM IDs"
+  description = "VM IDS"
   value       = [for vm in proxmox_vm_qemu.pvevm : vm.vmid]
+}
+
+output "vm_vcpus" {
+  description = "VM VCPUS"
+  value       = [for vm in proxmox_vm_qemu.pvevm : vm.vcpus]
+}
+
+output "vm_memory" {
+  description = "VM Memory"
+  value       = [for vm in proxmox_vm_qemu.pvevm : vm.memory]
+}
+
+output "vm_disk_size" {
+  description = "VM Disk Size"
+  value       = [for vm in proxmox_vm_qemu.pvevm : vm.disks.scsi.scsi0.disk.size]
 }
