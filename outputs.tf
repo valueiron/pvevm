d94ef16c64a0f8807e2b511ea30acc51c379ab8d
Matt <matt.mill@valueiron.com>
2025-04-12 17:52:22 -0600

    disk size change

--- outputs.tf
+++ outputs.tf
@@ -30,5 +30,5 @@ output "vm_memory" {
 
 output "vm_disk_size" {
   description = "VM disk size"
-  value       = [for vm in proxmox_vm_qemu.pvevm : vm.disks[0].size]
+  value       = [for vm in proxmox_vm_qemu.pvevm : vm.size]
 }
